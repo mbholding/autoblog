cb922f49f5c1b4a09aad3304748d2efc7b9af7e6
mbholding <mohcine.belcaid.pro@gmail.com>
2020-04-23 00:14:54 +0000

    bimber

--- wp-login.php
+++ wp-login.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 	<!--[if IE 8]>
-		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US" prefix="og: http://ogp.me/ns#">
 	<![endif]-->
 	<!--[if !(IE 8) ]><!-->
-		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" prefix="og: http://ogp.me/ns#">
 	<!--<![endif]-->
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; autoblog1 &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='https://mbholding.github.io/autoblog/wp-includes/css/dashicons.min.css?ver=5.4' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='https://mbholding.github.io/autoblog/wp-includes/css/buttons.min.css?ver=5.4' media='all' />
-<link rel='stylesheet' id='forms-css'  href='https://mbholding.github.io/autoblog/wp-admin/css/forms.min.css?ver=5.4' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='https://mbholding.github.io/autoblog/wp-admin/css/l10n.min.css?ver=5.4' media='all' />
-<link rel='stylesheet' id='login-css'  href='https://mbholding.github.io/autoblog/wp-admin/css/login.min.css?ver=5.4' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://mbholding.github.io/autoblog/wp-includes/css/dashicons.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://mbholding.github.io/autoblog/wp-includes/css/buttons.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://mbholding.github.io/autoblog/wp-admin/css/forms.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://mbholding.github.io/autoblog/wp-admin/css/l10n.min.css?ver=5.4' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://mbholding.github.io/autoblog/wp-admin/css/login.min.css?ver=5.4' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -58,29 +58,34 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog"><a href="https://mbholding.github.io/autoblog/">
 		&larr; Back to autoblog1		</a></p>
 			</div>
-	<script src='https://mbholding.github.io/autoblog/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
-<script src='https://mbholding.github.io/autoblog/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
-<script>
+	<script type='text/javascript' src='https://mbholding.github.io/autoblog/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='https://mbholding.github.io/autoblog/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
 var _zxcvbnSettings = {"src":"https:\/\/mbholding.github.io\/autoblog\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
 </script>
-<script src='https://mbholding.github.io/autoblog/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
-<script>
+<script type='text/javascript' src='https://mbholding.github.io/autoblog/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/* ]]> */
 </script>
-<script src='https://mbholding.github.io/autoblog/wp-admin/js/password-strength-meter.min.js?ver=5.4'></script>
-<script src='https://mbholding.github.io/autoblog/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
-<script>
+<script type='text/javascript' src='https://mbholding.github.io/autoblog/wp-admin/js/password-strength-meter.min.js?ver=5.4'></script>
+<script type='text/javascript' src='https://mbholding.github.io/autoblog/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/autoblog1\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
 </script>
-<script src='https://mbholding.github.io/autoblog/wp-includes/js/wp-util.min.js?ver=5.4'></script>
-<script>
+<script type='text/javascript' src='https://mbholding.github.io/autoblog/wp-includes/js/wp-util.min.js?ver=5.4'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
 var userProfileL10n = {"warn":"Your new password has not been saved.","warnWeak":"Confirm use of weak password","show":"Show","hide":"Hide","cancel":"Cancel","ariaShow":"Show password","ariaHide":"Hide password"};
+/* ]]> */
 </script>
-<script src='https://mbholding.github.io/autoblog/wp-admin/js/user-profile.min.js?ver=5.4'></script>
-	<script>
-	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
-	</script>
-		<div class="clear"></div>
+<script type='text/javascript' src='https://mbholding.github.io/autoblog/wp-admin/js/user-profile.min.js?ver=5.4'></script>
+	<div class="clear"></div>
 	</body>
 	</html>
 	
